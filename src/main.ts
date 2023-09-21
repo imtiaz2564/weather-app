e9cfaac0a3966bd7918b48ff7b4d3eb09be039f5
MD IMTIAZ AHMED <50336934+imtiaz2564@users.noreply.github.com>
2023-09-21 16:46:07 +0200

    Updated weather

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
 
   // Serve static files from the "public" directory
   app.use(express.static(path.join(__dirname, '..', 'public')));
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,17 +1,41 @@
-import { Controller,Get } from '@nestjs/common';
+// import { Controller,Get } from '@nestjs/common';
+
+// @Controller('weather')
+// export class WeatherController {
+//   @Get()
+//   getWeather() {
+//     const temperature = Math.floor(Math.random() * 30) + 10; // Random temperature between 10 and 40 degrees Celsius
+//     const conditions = ['Sunny', 'Cloudy', 'Rainy', 'Snowy'];
+//     const randomCondition = conditions[Math.floor(Math.random() * conditions.length)];
+
+//     return {
+//       temperature,
+//       condition: randomCondition,
+//     };
+//   }
+
+// }
+
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('weather')
 export class WeatherController {
   @Get()
   getWeather() {
-    const temperature = Math.floor(Math.random() * 30) + 10; // Random temperature between 10 and 40 degrees Celsius
-    const conditions = ['Sunny', 'Cloudy', 'Rainy', 'Snowy'];
-    const randomCondition = conditions[Math.floor(Math.random() * conditions.length)];
+    const numberOfEntries = 5; // You can specify the number of weather entries you want
+    const weatherData = [];
 
-    return {
-      temperature,
-      condition: randomCondition,
-    };
+    for (let i = 0; i < numberOfEntries; i++) {
+      const temperature = Math.floor(Math.random() * 30) + 10; // Random temperature between 10 and 40 degrees Celsius
+      const conditions = ['Sunny', 'Cloudy', 'Rainy', 'Snowy'];
+      const randomCondition = conditions[Math.floor(Math.random() * conditions.length)];
+
+      weatherData.push({
+        temperature,
+        condition: randomCondition,
+      });
+    }
+
+    return weatherData;
   }
-
 }
