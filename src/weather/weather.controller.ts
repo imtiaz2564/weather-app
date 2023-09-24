78897f80d8579189a84732e0415de8d014414e4a
MD IMTIAZ AHMED <50336934+imtiaz2564@users.noreply.github.com>
2023-09-24 21:13:10 +0200

    updated weather function

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,41 +1,19 @@
-// import { Controller,Get } from '@nestjs/common';
-
-// @Controller('weather')
-// export class WeatherController {
-//   @Get()
-//   getWeather() {
-//     const temperature = Math.floor(Math.random() * 30) + 10; // Random temperature between 10 and 40 degrees Celsius
-//     const conditions = ['Sunny', 'Cloudy', 'Rainy', 'Snowy'];
-//     const randomCondition = conditions[Math.floor(Math.random() * conditions.length)];
-
-//     return {
-//       temperature,
-//       condition: randomCondition,
-//     };
-//   }
-
-// }
-
-import { Controller, Get } from '@nestjs/common';
+import { Controller,Get } from '@nestjs/common';
 
 @Controller('weather')
 export class WeatherController {
   @Get()
   getWeather() {
-    const numberOfEntries = 5; // You can specify the number of weather entries you want
     const weatherData = [];
+    const temperature = Math.floor(Math.random() * 30) + 10; // Random temperature between 10 and 40 degrees Celsius
+    const conditions = ['Sunny', 'Cloudy', 'Rainy', 'Snowy'];
+    const randomCondition = conditions[Math.floor(Math.random() * conditions.length)];
 
-    for (let i = 0; i < numberOfEntries; i++) {
-      const temperature = Math.floor(Math.random() * 30) + 10; // Random temperature between 10 and 40 degrees Celsius
-      const conditions = ['Sunny', 'Cloudy', 'Rainy', 'Snowy'];
-      const randomCondition = conditions[Math.floor(Math.random() * conditions.length)];
-
-      weatherData.push({
-        temperature,
-        condition: randomCondition,
-      });
-    }
-
+    weatherData.push({
+      temperature,
+      condition: randomCondition,
+    });
     return weatherData;
   }
+
 }
